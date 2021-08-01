f7e205e784e4217ecf80c72ac0b5762a0a37fd6a
Robin Jacobsson <robin.jacobsson@savr.com>
2021-08-01 12:48:03 +0200

    Daily-site: Test tailwind in elm
    Added a test container element for the incrementer. Also testing some
    elm syntax that I've not used before.

--- packages/daily-site/src/daily-client/elm/Main.elm
+++ packages/daily-site/src/daily-client/elm/Main.elm
@@ -1,13 +1,9 @@
 module Main exposing (Msg(..), main, update, view)
 
 import Browser
-import Html exposing (button, div, text)
+import Html exposing (Html, button, div, text)
+import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
-
-
-main : Program () Int Msg
-main =
-    Browser.sandbox { init = 0, update = update, view = view }
 
 
 type Msg
@@ -15,7 +11,21 @@ type Msg
     | Decrement
 
 
-update : Msg -> number -> number
+type alias Model =
+    Int
+
+
+init : Model
+init =
+    0
+
+
+main : Program () Model Msg
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+update : Msg -> Int -> Int
 update msg model =
     case msg of
         Increment ->
@@ -25,10 +35,20 @@ update msg model =
             model - 1
 
 
-view : Int -> Html.Html Msg
-view model =
+layout : Html Msg -> Html Msg
+layout html =
+    div [ class "md:container md:mx-auto mt-16 border-solid border-2 border-red-300 rounded-md" ] [ html ]
+
+
+incrementer : Int -> Html Msg
+incrementer model =
     div []
         [ button [ onClick Decrement ] [ text "-" ]
         , div [] [ text (String.fromInt model) ]
         , button [ onClick Increment ] [ text "+" ]
         ]
+
+
+view : Int -> Html Msg
+view model =
+    model |> incrementer |> layout
