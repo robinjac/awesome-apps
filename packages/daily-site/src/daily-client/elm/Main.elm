8abc40daa970315fdab82284b74df52e3ddae4d0
Robin Jacobsson <robin.jacobsson@savr.com>
2021-08-04 20:54:19 +0200

    Daily-site: Import meta data into elm
    Render out the headers to test if import was successful.

--- packages/daily-site/src/daily-client/elm/Main.elm
+++ packages/daily-site/src/daily-client/elm/Main.elm
@@ -1,54 +1,80 @@
 module Main exposing (Msg(..), main, update, view)
 
 import Browser
-import Html exposing (Html, button, div, text)
+import Html exposing (Html, div, h1, text)
 import Html.Attributes exposing (class)
-import Html.Events exposing (onClick)
-
-
-type Msg
-    = Increment
-    | Decrement
 
 
 type alias Model =
-    Int
+    { id : Int
+    , projects : List Project
+    , host_repository : String
+    }
 
 
-init : Model
-init =
-    0
+type alias BranchData =
+    { name : String
+    , slug : String
+    , date : String
+    }
 
 
-main : Program () Model Msg
+type alias Branches =
+    { main : List BranchData
+    , release : List BranchData
+    , user : List BranchData
+    , other : List BranchData
+    }
+
+
+type alias Project =
+    { name : String
+    , repository : String
+    , branches : Branches
+    }
+
+
+type Msg
+    = NoOp
+
+
+init : Model -> ( Model, Cmd Msg )
+init model =
+    ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+main : Program Model Model Msg
 main =
-    Browser.sandbox { init = init, update = update, view = view }
+    Browser.element { init = init, subscriptions = subscriptions, update = update, view = view }
 
 
-update : Msg -> Int -> Int
-update msg model =
-    case msg of
-        Increment ->
-            model + 1
-
-        Decrement ->
-            model - 1
+update : Msg -> Model -> ( Model, Cmd Msg )
+update _ model =
+    ( model, Cmd.none )
 
 
-layout : Html Msg -> Html Msg
-layout html =
-    div [ class "md:container md:mx-auto mt-16 border-solid border-2 border-red-300 rounded-md" ] [ html ]
+layout : Model -> Html Msg
+layout model =
+    model
+        |> content
+        |> div [ class "md:container md:mx-auto mt-16 border-solid border-2 border-red-300 rounded-md" ]
 
 
-incrementer : Int -> Html Msg
-incrementer model =
-    div []
-        [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (String.fromInt model) ]
-        , button [ onClick Increment ] [ text "+" ]
-        ]
+content : Model -> List (Html Msg)
+content model =
+    List.map header model.projects
 
 
-view : Int -> Html Msg
+header : Project -> Html Msg
+header project =
+    div [] [ h1 [] [ text project.name ] ]
+
+
+view : Model -> Html Msg
 view model =
-    model |> incrementer |> layout
+    layout model
--- packages/daily-site/src/daily-client/main.ts
+++ packages/daily-site/src/daily-client/main.ts
@@ -1,6 +1,9 @@
 // @ts-ignore
 import { Elm } from "./elm/Main.elm";
 
+import view_state from "./test/view_state.json";
+
 Elm.Main.init({
+    flags: view_state,
     node: document.getElementById("app"),
 });
