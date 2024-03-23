8e02b7b77f7de8539b3bce718b8a9f38373c51f7
robin jacobsson <robin.jacobsson@kebne.se>
2024-03-23 15:47:12 +0100

    Daly-site: Improve typing

--- packages/daily-site/src/daily-client/main.ts
+++ packages/daily-site/src/daily-client/main.ts
@@ -3,7 +3,29 @@ import { Elm } from "./elm/Main.elm";
 
 import view_state from "./test/view_state.json";
 
+type Slug = `${string}-${string}${string}`;
+type BranchType = `${string}`;
+type Date = `${string}-${string}-${string} ${string}:${string}`;
+
+type Branch = {
+    name: string;
+    slug: Slug;
+    date: Date;
+};
+
+type Project = {
+    name: string;
+    repository: string;
+    branches: { [branchType: BranchType]: Branch[] };
+};
+
+type ViewState = {
+    id: number;
+    host_repository: string;
+    projects: Project[];
+};
+
 Elm.Main.init({
-    flags: view_state,
-    node: document.getElementById("app"),
+    flags: view_state as ViewState,
+    node: document.getElementById("app") as HTMLElement,
 });
